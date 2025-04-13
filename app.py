f23f66603e6ec40196dfd933af88dec2526320ab
yerdnA-A <andreycricco@gmail.com>
2025-04-13 10:00:18 -0300

    add porta

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def obrigado():
     return render_template('obrigado.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
